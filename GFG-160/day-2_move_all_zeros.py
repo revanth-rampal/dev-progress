f2f710823e456d3241521fc496af05f35e1659e6
Revanth  Rampal <revanthrampal@gmail.com>
2024-11-22 18:52:15 +0530

    DAY-3 GFG-160

--- GFG-160/day-2_move_all_zeros.py
+++ GFG-160/day-2_move_all_zeros.py
@@ -3,8 +3,8 @@
 class Solution:
 	def pushZerosToEnd(self,arr):
 	    n=len(arr)
-	    j=0
-	    for i in range(n):
+        j=0
+        for i in range(n):
 	        if arr[i] !=0:
 	            arr[j], arr[i] =arr[i],arr[j]
 	            j+=1
