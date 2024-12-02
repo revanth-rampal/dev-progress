89c229f4c06ae84e864c52df9236752e04e5025b
Revanth  Rampal <revanthrampal@gmail.com>
2024-12-03 04:00:01 +0530

    basic patterns alphabets and other designs

---
+++ Python/pattern-RightTriangleAlphabet.py
@@ -0,0 +1,5 @@
+for i in range(0,5+1):
+    for j in range(1,i+1):
+        c=64+i
+        print(chr(c),end='')
+    print()
---
+++ Python/pattern-triangleAlphContinuous.py
@@ -0,0 +1,8 @@
+c=0
+for i in range(1,5+1):
+    for j in range(1,i+1):
+        c+=1
+        print(chr(c+64), end='')
+    print()
+
+
