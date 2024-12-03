0fc7705828b69e958c2d8770ab8314478fe5fc8b
Revanth  Rampal <revanthrampal@gmail.com>
2024-12-03 15:32:54 +0530

    Completed section 10 in Krish Naik DSA

---
+++ Krish-Naik-DSA/EX-15-CtoF.py
@@ -0,0 +1,13 @@
+def celsius_to_fahrenheit(C):
+    """
+    Function to convert temperature from Celsius to Fahrenheit.
+    
+    Parameters:
+    C (float): The temperature in Celsius.
+    
+    Returns:
+    float: The temperature in Fahrenheit.
+    """
+    # Your code here
+    F = float((9/5 * C) + 32)
+    return F
