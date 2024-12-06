92cc42aa0ba7b8d24ca2984a5f4b28b0874b4d66
Revanth  Rampal <revanthrampal@gmail.com>
2024-12-06 22:03:30 +0530

    GFG day 17

---
+++ GFG-160/Day-17_ninRepeatingChar.py
@@ -0,0 +1,59 @@
+#User function Template for python3
+                return i
+
+
+class Solution:
+    
+    #Function to find the first non-repeating character in a string.
+    def nonRepeatingChar(self,s):
+        #code here
+        seen={}
+        for i in s:
+            if i in seen:
+                seen[i] +=1
+            else:
+                seen[i] = 1
+                
+                
+        for i in s:
+            if seen[i]==1:
+                return i
+        
+        return '$'
+    
+    
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+
+@atexit.register
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        s = str(input())
+        obj = Solution()
+        ans = obj.nonRepeatingChar(s)
+        if (ans != '$'):
+            print(ans)
+        else:
+            print(-1)
+
+        print("~")
+
+# } Driver Code Ends
