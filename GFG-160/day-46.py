1e5c5949abfb5672dfb8805069cbed8a50b6711d
Revanth  Rampal <revanthrampal@gmail.com>
2025-01-04 18:57:11 +0530

    day 46 of gfg 160

---
+++ GFG-160/day-46.py
@@ -0,0 +1,7 @@
+
+class Solution:    
+    #Function to return the count of number of elements in union of two arrays.
+    def findUnion(self, a, b):
+        # code here
+        return len(set(a+b))
+
